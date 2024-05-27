5ce21ae359b75108d1f6518af31afd2354f700ce
gabrielsalazar225 <165944712+gabrielsalazar225@users.noreply.github.com>
2024-05-27 17:56:55 -0400

    html and css almost done

---
+++ index.js
@@ -0,0 +1,8 @@
+// const for the button cryptocurrency
+const cryptoEl = document.querySelector('#btnEl1')
+// const for the button stocks
+const stocksEl = document.querySelector('#btnEl2')
+
+// Creation the add Event for cryptoEl
+
+// Getting the info from the API 
