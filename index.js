3412199e87f95e1594f3aaf7a9d60635c4b0a9da
gabrielsalazar225 <165944712+gabrielsalazar225@users.noreply.github.com>
2024-05-29 19:28:46 -0400

    pulling code

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
-<<<<<<< HEAD
-// const for the button cryptocurrency
-// const cryptoEl = document.querySelector('#btnEl1')
-// // const for the button stocks
-// const stocksEl = document.querySelector('#btnEl2')
 
-// fetching Seeking Alpha Finance 
-const url = 'https://coinranking1.p.rapidapi.com/stats?referenceCurrencyUuid=yhjMzLPhuIDl'
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': 'b838712149msh20436172aeaeb14p16cfa4jsn227b1dfecc4d',
+// 		'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
+// 	}
+// };
 
-fetch(url). 
-then(function (res) {
-  console.log(res)  
-    return res.json();
-}). then()
-=======
+// let cryptos
+// let crytpoDetails
+// let cryptoHistory
+
+// const fetchCryptos = async () => {
+//   try {
+//     const response = await fetch('https://coinranking1.p.rapidapi.com/coins?limit=100', options);
+//     cryptos = await response.json();
+//     console.log(cryptos);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+const url = 'https://coinranking1.p.rapidapi.com/stats?referenceCurrencyUuid=yhjMzLPhuIDl';
 const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'b838712149msh20436172aeaeb14p16cfa4jsn227b1dfecc4d',
-		'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
-	}
-};
+  method: 'GET',
+  headers: {
+  'x-rapidapi-key': 'd77e412cfcmsh4d1205175cfac65p14fc70jsnbf7a53468582',
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com' 
+}};
 
-let cryptos
-let crytpoDetails
-let cryptoHistory
-
-const fetchCryptos = async () => {
-  try {
-    const response = await fetch('https://coinranking1.p.rapidapi.com/coins?limit=100', options);
-    cryptos = await response.json();
-    console.log(cryptos);
-  } catch (error) {
-    console.error(error);
-  }
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} 
+catch (error) {
+	console.error(error);
 }
+
 
 /* const fetchCryptoDetails = async (coindId) => {
   try {
@@ -55,16 +59,16 @@ const fetchCryptoHistory = async (coindId, timePeriod) => {
   }
 } */
 
-document.addEventListener('DOMContentLoaded', fetchCryptos);
+// document.addEventListener('DOMContentLoaded', fetchCryptos);
 
-const crypto = () => {
-  // link global stats values to global stats box
-  const stats = cryptos.data.stats
-  const total = stats.total
-  const volume = stats.total24hVolume
-  const coins = stats.totalCoins
-  const exchanges = stats.totalExchanges
-  const cap = stats.totalMarketCap
-  const markets = stats.totalMarkets
-}
->>>>>>> 075bb722330cccdb0e5f7230758153c0df735e2d
+// const crypto = () => {
+//   // link global stats values to global stats box
+//   const stats = cryptos.data.stats
+//   const total = stats.total
+//   const volume = stats.total24hVolume
+//   const coins = stats.totalCoins
+//   const exchanges = stats.totalExchanges
+//   const cap = stats.totalMarketCap
+//   const markets = stats.totalMarkets
+// }
+
