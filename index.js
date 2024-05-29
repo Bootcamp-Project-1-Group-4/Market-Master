522517a28bcd0b1702917ad07b45b409244d30c3
gabrielsalazar225 <165944712+gabrielsalazar225@users.noreply.github.com>
2024-05-29 17:51:16 -0400

    pulling index

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // const for the button cryptocurrency
-const cryptoEl = document.querySelector('#btnEl1')
-// const for the button stocks
-const stocksEl = document.querySelector('#btnEl2')
+// const cryptoEl = document.querySelector('#btnEl1')
+// // const for the button stocks
+// const stocksEl = document.querySelector('#btnEl2')
 
-// Creation the add Event for cryptoEl
+// fetching Seeking Alpha Finance 
+const url = 'https://coinranking1.p.rapidapi.com/stats?referenceCurrencyUuid=yhjMzLPhuIDl'
 
-// Getting the info from the API 
+fetch(url). 
+then(function (res) {
+  console.log(res)  
+    return res.json();
+}). then()
