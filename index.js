53603dd8bf05729c7d66c5b0f9c06f00a0b14e36
gabrielsalazar225 <165944712+gabrielsalazar225@users.noreply.github.com>
2024-05-31 18:32:02 -0400

    pulling updated version

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-const options = {
-  method: 'GET',
-  headers: {
-    'x-rapidapi-key': 'b838712149msh20436172aeaeb14p16cfa4jsn227b1dfecc4d',
-    'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
-  }
-};
 const fetchCryptos = async () => {
   try {
     const response = await fetch('https://coinranking1.p.rapidapi.com/coins?limit=100', options);
     const json = await response.json();
     const data = json.data
     const stats = data.stats
-    console.log(stats)
-    document.getElementById('cryptos').innerText=`$${stats.total}`
-    document.getElementById('markets').innerText=`$${stats.totalMarkets}`
-    document.getElementById('exchanges').innerText=`$${stats.totalExchanges}`
-    document.getElementById('market-cap').innerText=`$${Math.round(stats.totalMarketCap/1000000000)} Billion`
-    document.getElementById('volume').innerText=`$${Math.round(stats.total24hVolume/1000000000)} Billion`
+    const coins = data.coins
+    console.log(data)
+    document.getElementById('cryptos').innerText=`$${stats.total.toLocaleString()}`
+    document.getElementById('markets').innerText=`$${stats.totalMarkets.toLocaleString()}`
+    document.getElementById('exchanges').innerText=`$${stats.totalExchanges.toLocaleString()}`
+    document.getElementById('market-cap').innerText=`$${Math.round(stats.totalMarketCap/1000000000).toLocaleString()} Billion`
+    document.getElementById('volume').innerText=`$${Math.round(stats.total24hVolume/1000000000).toLocaleString()} Billion`
+    
+    const coinCards = coins.map((coin)=>{
+      return (
+        `<div class="coin-card">
+          <header>
+            <h2>${coin.name}</h2>
+            <img src=${coin.iconUrl}></img>
+          </header>
+          <p>Price: $${Math.round(coin.price).toLocaleString()}</p>
+          <p>Market Cap: $${Math.round(coin.marketCap/1000000000).toLocaleString()} Billion</p>
+          <p>Daily Cap: ${coin.change}%</p>
+        </div>`
+      )
+    }).join('')
+
+    console.log(coinCards)
+    document.getElementById('crypto').innerHTML = coinCards
   } catch (error) {
     console.error(error);
   }
 }
+
+
 document.addEventListener("DOMContentLoaded", fetchCryptos);
-/* const fetchCryptoDetails = async (coindId) => {
+
+const fetchCryptoDetails = async (coindId) => {
   try {
     const response = await fetch(`https://coinranking1.p.rapidapi.com/coin/${coinId}`, options);
     crytpoDetails = await response.json();
@@ -31,6 +45,7 @@ document.addEventListener("DOMContentLoaded", fetchCryptos);
     console.error(error);
   }
 }
+
 const fetchCryptoHistory = async (coindId, timePeriod) => {
   try {
     const response = await fetch(`https://coinranking1.p.rapidapi.com/coin/${coinId}/history?timePeriod=${timePeriod}`, options);
@@ -39,4 +54,4 @@ const fetchCryptoHistory = async (coindId, timePeriod) => {
   } catch (error) {
     console.error(error);
   }
-} */
+}
